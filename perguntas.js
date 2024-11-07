b7fc13965fee45833edfd16909ffd7cc2637c48e
hihiKKKka <mhlk8386@gmail.com>
2024-11-07 16:18:28 -0300

    primeira versão do flash card

---
+++ perguntas.js
@@ -0,0 +1,49 @@
+criaCartao(
+    "Crazy Frog", //categoria//
+    "Aos 1:20 da musica Axel F, o que Crazy Frog diz?",  //pergunta//
+    "Bara Ba Bem Ba Brea Ba Brea Brea Breo Brem Bra Bram Bram Ba Ba Ba Ba Beeeeeeeeeeeeeeeeeeeeeeeeeeeee"   //resposta//
+)
+
+criaCartao(
+    "Hello Kit",
+    "Onde Hello Kit mora?",
+    "Hello Kit mora em Londres, na Inglaterra com seus pais e sua irmã gêmea"
+)
+criaCartao(
+    "Músicas",
+    "O que é dito aos 2:33 da música 'Ghost Town' de Kanye West?",
+    "Woah<br> Once again, I am a child<br> I let it all go (go)<Br>Of everything that I know, yeah<Br>Of everything that I know, yeah"
+)
+
+criaCartao(
+    "contexto",
+    "Qual era a palavra #333 do site Contexto?",
+    "MAÇÃ"
+    
+)
+criaCartao(
+    "Ben 10",
+    "Qual o alien de número 59 do Ben 10?",
+    "Atômico"
+)
+criaCartao(
+    "Animais",
+    "Quem vence Águia Americana🙄👎<Br>ou<Br>Harpía Brasileira😎👍",
+    "🔥🔥🔥HARPÍA BRASILEIRA🔥🔥🔥"
+
+)
+criaCartao(
+    "Filmes",
+    "Qual a primeira regra do Clube da Luta?🧼🤼‍♂️",
+    "Se divirta e seja você mesmo🥰"
+)
+criaCartao(
+    "Músicas",
+    "A quem a música 'Lotus 72D' faz homenagem?",
+    "A música Lotus 72D faz uma homenagem ao piloto brasileiro Emerson Fittipaldi"
+)
+criaCartao(
+    "Filmes",
+    "O que um negro que esta casado com uma velha<Br> fala para você após você tirar uma foto dele?",
+    "Corra😨😧😨"
+)
